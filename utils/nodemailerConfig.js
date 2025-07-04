502f6815d2fb111a8a2b0c3389a45c29eb9cffec
francastillo55 <francarlos_65@hotmail.com>
2025-07-04 11:36:13 -0400

    Primer commit

---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,31 @@
+// module.exports = {
+//   host: "smtp.ethereal.email",
+//   port: 587,
+//   auth: {
+//     user: "adrianna.dubuque42@ethereal.email",
+//     pass: "tKch2Kr2eFEukxREZz",
+//   },
+// };
+
+/////////////////////////////////////////
+module.exports = {
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "vincenzo.kassulke63@ethereal.email",
+    pass: "2WdfgxKvbjkus91cVY",
+  },
+};
+/////////////////////////////////////////
+
+// module.exports = {
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, // true para el puerto 465
+//   auth: {
+//     user: "francarlos1082@gmail.com",
+//     pass: "zuoevuzpwfbrwetp",
+//   },
+// };
+
+//zuoe vuzp wfbr wetp
