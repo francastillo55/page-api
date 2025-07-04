fdd313b1871e4ed8d45ae36ae72de9ca7bc4bc25
francastillo55 <francarlos_65@hotmail.com>
2025-07-04 12:04:46 -0400

    Agregar mail de produccion

--- utils/nodemailerConfig.js
+++ utils/nodemailerConfig.js
@@ -8,24 +8,24 @@
 // };
 
 /////////////////////////////////////////
-module.exports = {
-  host: "smtp.ethereal.email",
-  port: 587,
-  auth: {
-    user: "vincenzo.kassulke63@ethereal.email",
-    pass: "2WdfgxKvbjkus91cVY",
-  },
-};
-/////////////////////////////////////////
-
 // module.exports = {
-//   host: "smtp.gmail.com",
+//   host: "smtp.ethereal.email",
 //   port: 587,
-//   secure: false, // true para el puerto 465
 //   auth: {
-//     user: "francarlos1082@gmail.com",
-//     pass: "zuoevuzpwfbrwetp",
+//     user: "vincenzo.kassulke63@ethereal.email",
+//     pass: "2WdfgxKvbjkus91cVY",
 //   },
 // };
+/////////////////////////////////////////
+
+module.exports = {
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true para el puerto 465
+  auth: {
+    user: "francarlos1082@gmail.com",
+    pass: "zuoevuzpwfbrwetp",
+  },
+};
 
 //zuoe vuzp wfbr wetp
