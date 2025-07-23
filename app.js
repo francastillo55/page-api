304e6806c2292a097e3f3d3f04549a3f2f0581fa
francastillo55 <francarlos_65@hotmail.com>
2025-07-22 21:54:34 -0400

    PATCH was added as Access-Control

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(
   cors({
     origin: "https://wordle.franccr.com",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
